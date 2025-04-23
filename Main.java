f92d36d776cf017d20d6025f9a95d21d06c86560
sonamu <skit21sol@gmal.com>
2025-04-23 09:34:45 +0900

    five

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
       System.out.println("임예솔");
       World.run();
       Hello.run();
+      World.run2();
     }
   }
--- World.java
+++ World.java
@@ -1,5 +1,10 @@
 public class World {
     public static void run() {
-      System.out.println("202514099");
+      System.out.println("202114043");
+    }
+  
+    public static void run2() {
+      System.out.println("hello world");
     }
   }
+  
